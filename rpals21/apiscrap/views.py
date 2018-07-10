9c47a806f7f81776bccaf6a963aaee3bf016f987
Fredy Mendoza Vargas <fredymv03@gmail.com>
2018-07-10 16:44:21 -0500

    Añadiendo y creando las rutas y viewset

--- rpals21/apiscrap/views.py
+++ rpals21/apiscrap/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Robot
+from .serializers import RobotModelSerializer
+
+
+class RobotModelViewSet(ModelViewSet):
+    queryset = Robot.objects.all()
+    serializer_class = RobotModelSerializer
